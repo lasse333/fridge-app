fd2fb43b52a0d40fa1c19b9a25c2289b875af6a1
Lasse333 <reneskjodt333@gmail.com>
2023-03-24 17:05:32 +0000

    Update AddItem.js and Item.js

--- assets/js/pages/AddItem.js
+++ assets/js/pages/AddItem.js
@@ -1,88 +1,179 @@
-import { clearChildren, changeUrlPath, createElement } from "../FastHTML.js"
-import { camera, startVideo } from "../components/camera.js"
-import { saveItem, sortItems } from "../components/localstorage.js"
-import Fridge from "./Fridge.js"
-import createUUID from "../components/uuid.js"
-import { getLang, langText } from "../components/language.js"
+import { clearChildren, changeUrlPath, createElement } from "../FastHTML.js";
+import { camera, startVideo } from "../components/camera.js";
+import { saveItem, sortItems } from "../components/localstorage.js";
+import Fridge from "./Fridge.js";
+import createUUID from "../components/uuid.js";
+import { getLang, langText } from "../components/language.js";
 
 export default function Add() {
-    clearChildren("main")
-    changeUrlPath("/add")
-    window.onclick()
+  clearChildren("main");
+  changeUrlPath("/add");
+  window.onclick();
 
-    const photo = createElement("img", {style: {
+  const photo = createElement("img", {
+    style: {
+      width: "100%",
+      margin: "0 auto 20px",
+      display: "block",
+    },
+  });
+
+  const imageInput = createElement("input", {
+    type: "hidden",
+    name: "ImageData",
+    required: true,
+  });
+  const experationDateTimestamp = createElement("input", {
+    type: "hidden",
+    name: "ExperationDate",
+    required: false,
+  });
+  const amountInput = createElement("input", {
+    type: "number",
+    step: 1,
+    min: 1,
+    required: true,
+    placeholder: langText[getLang()]["AddItem"]["amountInput"],
+    name: "ItemAmount",
+    value: 1,
+  });
+
+  const formular = createElement(
+    "form",
+    {
+      onsubmit: function (e) {
+        e.preventDefault(); /*console.log(e)*/
+      },
+      className: "formular",
+      autocomplete: "off",
+      style: {
+        display: "flex",
+        flexDirection: "column",
         width: "100%",
-        margin: "0 auto 20px",
-        display: "block"
-    }})
-
-    const imageInput = createElement("input", {type: "hidden", name: "ImageData", required: true})
-    const experationDateTimestamp = createElement("input", {type: "hidden", name: "ExperationDate", required: false})
-    const amountInput = createElement("input", {type: "number", step: 1, min: 1, required: true, placeholder: langText[getLang()]["AddItem"]["amountInput"], name: "ItemAmount", value: 1})
-
-    const formular = createElement("form", {
-        onsubmit: function(e) {e.preventDefault();/*console.log(e)*/},
-        className: "formular",
-        autocomplete: "off",
+        padding: "0 20px",
+        gap: "20px",
+      },
+    },
+    [
+      imageInput,
+      createElement("label", {
+        innerText: langText[getLang()]["AddItem"]["nameInput"] + ":",
+      }),
+      createElement("input", {
+        type: "text",
+        placeholder: langText[getLang()]["AddItem"]["nameInput"],
+        name: "ItemName",
+        required: true,
+      }),
+      createElement(
+        "label",
+        {
+          innerText:
+            langText[getLang()]["AddItem"]["expirationDateInput"] + ":",
+        },
+        []
+      ),
+      createElement("input", {
+        type: "date",
+        placeholder: langText[getLang()]["AddItem"]["expirationDateInput"],
+        required: false,
+        onchange: function (e) {
+          experationDateTimestamp.value = Date.parse(e.target.value);
+        },
+      }),
+      experationDateTimestamp,
+      createElement("label", {
+        innerText: langText[getLang()]["AddItem"]["amountInput"] + ":",
+      }),
+      amountInput,
+      createElement(
+        "div",
+        {
+          style: {
+            display: "flex",
+            gap: "10px",
+          },
+        },
+        [
+          createElement(
+            "button",
+            {
+              onclick: function (e) {
+                e.preventDefault();
+                amountInput.value = +amountInput.value + 1;
+              },
+              style: {
+                width: "100%",
+                backgroundColor: "#5c0",
+              },
+            },
+            [
+              createElement("img", {
+                src: "/fridge-app/assets/icons/Add.svg",
+                style: { height: "20px", width: "20px" },
+              }),
+            ]
+          ),
+          createElement(
+            "button",
+            {
+              onclick: function (e) {
+                e.preventDefault();
+                if (+amountInput.value > +amountInput.min)
+                  amountInput.value = +amountInput.value - 1;
+              },
+              style: {
+                width: "100%",
+                backgroundColor: "#c20",
+              },
+            },
+            [
+              createElement("img", {
+                src: "/fridge-app/assets/icons/Remove.svg",
+                style: { height: "20px", width: "20px" },
+              }),
+            ]
+          ),
+        ]
+      ),
+      createElement("br"),
+      createElement("input", {
+        type: "submit",
+        value: langText[getLang()]["AddItem"]["submitButton"],
         style: {
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-            padding: "0 20px",
-            gap: "20px"
-        }
-    }, [
-        imageInput,
-        createElement("label", {innerText: langText[getLang()]["AddItem"]["nameInput"] + ":"}),
-        createElement("input", {type: "text", placeholder: langText[getLang()]["AddItem"]["nameInput"], name: "ItemName", required: true}),
-        createElement("label", {innerText: langText[getLang()]["AddItem"]["expirationDateInput"] + ":"}, []),
-        createElement("input", {type: "date", placeholder: langText[getLang()]["AddItem"]["expirationDateInput"], required: false, onchange: function(e) {
-            experationDateTimestamp.value = Date.parse(e.target.value)
-        }}),
-        experationDateTimestamp,
-        createElement("label", {innerText: langText[getLang()]["AddItem"]["amountInput"] + ":"}),
-        amountInput,
-        createElement("div", {style: {
-            display: "flex",
-            gap: "10px"
-        }}, [
-            createElement("button",{onclick: function(e) {
-                e.preventDefault()
-                amountInput.value = +amountInput.value + 1
-            }, style: {
-                width: "100%",
-                backgroundColor: "#5c0"
-            }}, [createElement("img", {src: "../assets/icons/Add.svg", style: {height: "20px", width: "20px"}})]),
-            createElement("button",{onclick: function(e) {
-                e.preventDefault()
-                if (+amountInput.value > +amountInput.min) amountInput.value = +amountInput.value - 1
-            }, style: {
-                width: "100%",
-                backgroundColor: "#c20"
-            }}, [createElement("img", {src: "../assets/icons/Remove.svg", style: {height: "20px", width: "20px"}})])
-        ]),
-        createElement("br"),
-        createElement("input", {type: "submit", value: langText[getLang()]["AddItem"]["submitButton"], style: {
-            fontSize: "20px",
-            border: "none",
-            color: "#fff",
-            fontWeight: "bold",
-            backgroundColor: "#05f",
-            whiteSpace: "break-spaces"
-        }})
-    ])
+          fontSize: "20px",
+          border: "none",
+          color: "#fff",
+          fontWeight: "bold",
+          backgroundColor: "#05f",
+          whiteSpace: "break-spaces",
+        },
+      }),
+    ]
+  );
 
-    const imageInputForm = (imgBase64) => {
-
-        photo.setAttribute('src', "data:image/*;base64," + imgBase64);
-        imageInput.value = imgBase64
-        clearChildren("main")
-        document.querySelector("main").appendChild(createElement("div", {style: {
+  const imageInputForm = (imgBase64) => {
+    photo.setAttribute("src", "data:image/*;base64," + imgBase64);
+    imageInput.value = imgBase64;
+    clearChildren("main");
+    document.querySelector("main").appendChild(
+      createElement(
+        "div",
+        {
+          style: {
             height: "100%",
             width: "100%",
-            padding: "20px 0"
-        }}, [
-            createElement("button", {onclick: function() {startVideo(imageInputForm)}, style: {
+            padding: "20px 0",
+          },
+        },
+        [
+          createElement(
+            "button",
+            {
+              onclick: function () {
+                startVideo(imageInputForm);
+              },
+              style: {
                 width: "calc(100% - 20px)",
                 maxWidth: "50vmin",
                 margin: "0 auto",
@@ -90,40 +181,44 @@ export default function Add() {
                 padding: "0",
                 border: "none",
                 outline: "none",
-                background: "none"
-            }},[photo]),
-            formular
-        ]))
+                background: "none",
+              },
+            },
+            [photo]
+          ),
+          formular,
+        ]
+      )
+    );
 
-        formular.onsubmit = function(e) {
-            e.preventDefault()
-            let formData = {}
-            for (let i=0;i<e.target.children.length;i++) {
-                let child = e.target.children[i]
-                if (child.name && child.tagName.toLowerCase() == "input") {
-                    formData[child.name] = child.value
-                }
-            }
-
-            formData.uuid = createUUID()
-
-            // init()
-            // write(formData)
-            // db.close()
-            saveItem(formData)
-            sortItems()
-
-            for (let i=0;i<e.target.children.length;i++) {
-                let child = e.target.children[i]
-                if (child.tagName.toLowerCase() == "input" && child.type != "submit") {
-                    child.value = ""
-                }
-            }
-
-            Fridge()
+    formular.onsubmit = function (e) {
+      e.preventDefault();
+      let formData = {};
+      for (let i = 0; i < e.target.children.length; i++) {
+        let child = e.target.children[i];
+        if (child.name && child.tagName.toLowerCase() == "input") {
+          formData[child.name] = child.value;
         }
+      }
 
-    }
+      formData.uuid = createUUID();
 
-    camera(imageInputForm)
+      // init()
+      // write(formData)
+      // db.close()
+      saveItem(formData);
+      sortItems();
+
+      for (let i = 0; i < e.target.children.length; i++) {
+        let child = e.target.children[i];
+        if (child.tagName.toLowerCase() == "input" && child.type != "submit") {
+          child.value = "";
+        }
+      }
+
+      Fridge();
+    };
+  };
+
+  camera(imageInputForm);
 }
--- assets/js/pages/Item.js
+++ assets/js/pages/Item.js
@@ -1,104 +1,209 @@
-import { convertReadable, toDateString} from "../components/datetime.js";
+import { convertReadable, toDateString } from "../components/datetime.js";
 import { getLang, langText } from "../components/language.js";
-import { getItems, removeItem, sortItems, updateItem } from "../components/localstorage.js";
+import {
+  getItems,
+  removeItem,
+  sortItems,
+  updateItem,
+} from "../components/localstorage.js";
 import { changeUrlPath, clearChildren, createElement } from "../FastHTML.js";
 import Fridge from "./Fridge.js";
 
 export default function Item(i) {
+  if (i == null) Fridge();
 
-    if (i == null) Fridge()
+  sortItems();
+  let items = getItems();
 
-    sortItems()
-    let items = getItems()
+  clearChildren("main");
+  changeUrlPath("/item/" + i);
 
-    clearChildren("main")
-    changeUrlPath("/item/" + i)
-
-    const removeItemButton = createElement("button", {innerText: langText[getLang()]["Item"]["removeButton"],disabled: true, onclick: function() {
-        if (confirm(langText[getLang()]["Item"]["confirm1"] + items[i].ItemName + langText[getLang()]["Item"]["confirm2"])) removeItem(i)
-        Fridge()
+  const removeItemButton = createElement("button", {
+    innerText: langText[getLang()]["Item"]["removeButton"],
+    disabled: true,
+    onclick: function () {
+      if (
+        confirm(
+          langText[getLang()]["Item"]["confirm1"] +
+            items[i].ItemName +
+            langText[getLang()]["Item"]["confirm2"]
+        )
+      )
+        removeItem(i);
+      Fridge();
     },
     style: {
-        width: "100%",
-        backgroundColor: "#c20",
-        padding: "5px auto",
-        color: "#fff",
-        fontWeight: "bold",
-        fontSize: "20px"
-    }})
+      width: "100%",
+      backgroundColor: "#c20",
+      padding: "5px auto",
+      color: "#fff",
+      fontWeight: "bold",
+      fontSize: "20px",
+    },
+  });
 
-    document.querySelector("main").appendChild(createElement("div", {style: {
-        display: "grid",
-        gridTemplateRows: "60px auto"
-    }}, [
-        createElement("header", {style: {
-            backgroundColor: "#222",
-            display: "flex"
-        }}, [
-            createElement("button", {onclick: Fridge ,style: {
-                margin: "0",
-                background: "none",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                outline: "none"
-            }}, [
-                createElement("img", {src: "../assets/icons/Back.svg", style: {
+  document.querySelector("main").appendChild(
+    createElement(
+      "div",
+      {
+        style: {
+          display: "grid",
+          gridTemplateRows: "60px auto",
+        },
+      },
+      [
+        createElement(
+          "header",
+          {
+            style: {
+              backgroundColor: "#222",
+              display: "flex",
+            },
+          },
+          [
+            createElement(
+              "button",
+              {
+                onclick: Fridge,
+                style: {
+                  margin: "0",
+                  background: "none",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  outline: "none",
+                },
+              },
+              [
+                createElement("img", {
+                  src: "../assets/icons/Back.svg",
+                  style: {
                     height: "30px",
-                    width: "30px"
-                }})
-            ]),
-            createElement("h1", {innerText: items[i].ItemName, style: {
+                    width: "30px",
+                  },
+                }),
+              ]
+            ),
+            createElement("h1", {
+              innerText: items[i].ItemName,
+              style: {
                 display: "flex",
-                alignItems: "center"
-            }})
-        ]),
-        createElement("img", {src: "data:image/*;base64,"+ items[i].ImageData, style: {
+                alignItems: "center",
+              },
+            }),
+          ]
+        ),
+        createElement("img", {
+          src: "data:image/*;base64," + items[i].ImageData,
+          style: {
             width: "100%",
             maxWidth: "500px",
-            margin: "0 auto"
-        }}),
-        createElement("div", {style: {
-            padding: "20px",
-            display: "flex",
-            flexDirection: "column",
-            gap: "10px"
-        }}, [
-            items[i].ExperationDate != "" ? createElement("h3", {innerText: langText[getLang()]["Item"]["dateLabel"] + ": " + (toDateString(items[i].ExperationDate,langText[getLang()]["Item"]["locale"]))}):null,
-            createElement("h3", {innerText: langText[getLang()]["Item"]["timeLabel"] + ": " + (items[i].ExperationDate == "" ? langText[getLang()]["Item"]["unknown"] : convertReadable(items[i].ExperationDate) || langText[getLang()]["Item"]["expired"])}),
+            margin: "0 auto",
+          },
+        }),
+        createElement(
+          "div",
+          {
+            style: {
+              padding: "20px",
+              display: "flex",
+              flexDirection: "column",
+              gap: "10px",
+            },
+          },
+          [
+            items[i].ExperationDate != ""
+              ? createElement("h3", {
+                  innerText:
+                    langText[getLang()]["Item"]["dateLabel"] +
+                    ": " +
+                    toDateString(
+                      items[i].ExperationDate,
+                      langText[getLang()]["Item"]["locale"]
+                    ),
+                })
+              : null,
+            createElement("h3", {
+              innerText:
+                langText[getLang()]["Item"]["timeLabel"] +
+                ": " +
+                (items[i].ExperationDate == ""
+                  ? langText[getLang()]["Item"]["unknown"]
+                  : convertReadable(items[i].ExperationDate) ||
+                    langText[getLang()]["Item"]["expired"]),
+            }),
             createElement("br"),
-            createElement("h3", {innerText: langText[getLang()]["Item"]["amountLabel"] + ": " + items[i].ItemAmount}),
-            createElement("div", {style: {
-                display: "flex",
-                gap: "10px"
-            }}, [
-                createElement("button", {style: {
-                    width: "100%",
-                    backgroundColor: "#5c0",
-                    padding: "5px auto"
-                }, onclick: function() {
-                    updateItem(i, {ItemAmount: (+items[i].ItemAmount + 1)+""})
-                    Item(i)
-                }}, [
-                    createElement("img", {src: "../assets/icons/Add.svg", style: {height: "20px", width: "20px"}})
-                ]),
-                createElement("button", {style: {
-                    width: "100%",
-                    backgroundColor: "#c20",
-                    padding: "5px auto"
-                }, onclick: function() {
-                    if (+items[i].ItemAmount > 1) updateItem(i, {ItemAmount: (+items[i].ItemAmount - 1)+""})
-                    Item(i)
-                }}, [
-                    createElement("img", {src: "../assets/icons/Remove.svg", style: {height: "20px", width: "20px"}})
-                ])
-            ]),
+            createElement("h3", {
+              innerText:
+                langText[getLang()]["Item"]["amountLabel"] +
+                ": " +
+                items[i].ItemAmount,
+            }),
+            createElement(
+              "div",
+              {
+                style: {
+                  display: "flex",
+                  gap: "10px",
+                },
+              },
+              [
+                createElement(
+                  "button",
+                  {
+                    style: {
+                      width: "100%",
+                      backgroundColor: "#5c0",
+                      padding: "5px auto",
+                    },
+                    onclick: function () {
+                      updateItem(i, {
+                        ItemAmount: +items[i].ItemAmount + 1 + "",
+                      });
+                      Item(i);
+                    },
+                  },
+                  [
+                    createElement("img", {
+                      src: "/fridge-app/assets/icons/Add.svg",
+                      style: { height: "20px", width: "20px" },
+                    }),
+                  ]
+                ),
+                createElement(
+                  "button",
+                  {
+                    style: {
+                      width: "100%",
+                      backgroundColor: "#c20",
+                      padding: "5px auto",
+                    },
+                    onclick: function () {
+                      if (+items[i].ItemAmount > 1)
+                        updateItem(i, {
+                          ItemAmount: +items[i].ItemAmount - 1 + "",
+                        });
+                      Item(i);
+                    },
+                  },
+                  [
+                    createElement("img", {
+                      src: "/fridge-app/assets/icons/Remove.svg",
+                      style: { height: "20px", width: "20px" },
+                    }),
+                  ]
+                ),
+              ]
+            ),
             createElement("br"),
-            removeItemButton
-        ])
-    ]))
+            removeItemButton,
+          ]
+        ),
+      ]
+    )
+  );
 
-    setTimeout(function() {
-        removeItemButton.disabled = false
-    }, 3*1000)
+  setTimeout(function () {
+    removeItemButton.disabled = false;
+  }, 3 * 1000);
 }
