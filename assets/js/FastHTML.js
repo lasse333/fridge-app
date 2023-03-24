a8fd673ab58d93fb1761dc51ca275ac0100c024f
Lasse333 <reneskjodt333@gmail.com>
2023-03-24 16:42:37 +0000

    Update FastHTML.js and index.html

--- assets/js/FastHTML.js
+++ assets/js/FastHTML.js
@@ -1,50 +1,50 @@
 export function openFullscreen(element) {
-    if (element.requestFullscreen) {
-        element.requestFullscreen();
-    } else if (element.webkitRequestFullscreen) { /* Safari */
-        element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) { /* IE11 */
-        elem.msRequestFullscreen();
-    }
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    /* Safari */
+    element.webkitRequestFullscreen();
+  } else if (element.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
 }
 
 export function createElement(type, attributesIn, childrenIn) {
-    let attributes = attributesIn ?? {}
-    let children = childrenIn ?? []
+  let attributes = attributesIn ?? {};
+  let children = childrenIn ?? [];
 
-    let element = document.createElement(type)
-    Object.assign(element, attributes)
+  let element = document.createElement(type);
+  Object.assign(element, attributes);
 
-    if (attributes.style) Object.assign(element.style, attributes.style)
+  if (attributes.style) Object.assign(element.style, attributes.style);
 
-    children.forEach(e => {
-        if (e == null) return
-        element.appendChild(e)
-    })
+  children.forEach((e) => {
+    if (e == null) return;
+    element.appendChild(e);
+  });
 
-    return element
+  return element;
 }
 
 export function changeStyle(selectors, style) {
-    document.querySelectorAll(selectors).forEach((element) => {
-        Object.assign(element.style, style)
-    })
+  document.querySelectorAll(selectors).forEach((element) => {
+    Object.assign(element.style, style);
+  });
 }
 
 export function addChildren(element, children) {
-    children.forEach(child => element.appendChild(child))
+  children.forEach((child) => element.appendChild(child));
 
-    return element
+  return element;
 }
 
 export function changeUrlPath(path) {
-
-    window.history.pushState("", "", path);
-
+  window.history.pushState("", "", "/fridge-app" + path);
 }
 
 export function clearChildren(selectors) {
-    document.querySelectorAll(selectors).forEach((element) => {
-        element.innerHTML = ""
-    })
+  document.querySelectorAll(selectors).forEach((element) => {
+    element.innerHTML = "";
+  });
 }
