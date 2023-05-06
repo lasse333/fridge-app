4365f1c50c19939065a1c90204c9a0398b405d74
Lasse333 <reneskjodt333@gmail.com>
2023-05-06 15:19:24 +0000

    Update add.css and Add.js

--- assets/js/pages/Add.js
+++ assets/js/pages/Add.js
@@ -6,16 +6,23 @@ export default async function Add() {
   let camera = new Camera({
     video: { width: 200, height: 200, facingMode: "environment" },
   });
+
+  async function takePicture() {
+    console.log(await camera.takePicture());
+    camera.stopCamera();
+    cameraPopup.close();
+  }
+
   let cameraPopup = createElement("dialog", { class: "cameraOverlay" }, [
-    camera.videoElement,
-    createElement("button", {
-      innerText: "Take Picture",
-      onclick: async function () {
-        console.log(await camera.takePicture());
-        camera.stopCamera();
-        cameraPopup.close();
-      },
-    }),
+    createElement("div", { class: "cameraContainer" }, [
+      createElement("button", { class: "cameraFeed", onclick: takePicture }, [
+        camera.videoElement,
+      ]),
+      createElement("button", {
+        innerText: "Take Picture",
+        onclick: takePicture,
+      }),
+    ]),
   ]);
 
   return createElement("main", { style: AddStyle }, [
